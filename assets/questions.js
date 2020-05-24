b667e927fc97bd49897c0060e175156fe6b0a98c
Izuku <63758052+Beki-G@users.noreply.github.com>
2020-05-24 16:58:42 -0700

    Initial set up of html, css & js files

---
+++ assets/questions.js
@@ -0,0 +1,17 @@
+const questionArr = [
+    {
+        question: "What color is the ocean?",
+        option1: "blue",
+        option2: "orange",
+        option3:"yellow",
+        answer: "blue"
+    },
+    {
+        question: "What color is the sky?",
+        option1: "orange",
+        option2: "pink",
+        option3: "blue",
+        answer: "blue"
+    }
+
+]
